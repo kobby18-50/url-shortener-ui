daa84668fb0cffb2b13cfeb27dc53a8e686d457e
Kwabena <kobbyokai18@gmail.com>
2026-02-22 13:31:43 +0000

    fix: add URL validation to longUrl in schema

--- src/components/schema/urlSchema.ts
+++ src/components/schema/urlSchema.ts
@@ -4,5 +4,6 @@ export const urlValidationSchema = yup.object({
      longUrl: yup
     .string()
     .required("Url is required")
-    .min(10, "Url must be at least 10 characters"),
+    .min(10, "Url must be at least 10 characters")
+    .url('Enter a valid url')
 })
